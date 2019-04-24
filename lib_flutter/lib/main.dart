31891f8ce08c82dbfe9d61248b967560c9eb11f7
loading <dwzzhw@126.com>
2019-04-24 22:11:39 +0800

    add standalone tiny sports project

--- lib_flutter/lib/main.dart
+++ lib_flutter/lib/main.dart
@@ -9,6 +9,7 @@ void main() {
 //  DartGrammarTest().doFutureTest();
   llogd('Flutter:main()', 'route name=${window.defaultRouteName}');
   if (window.defaultRouteName == '999' ||
+      window.defaultRouteName == '/' ||
 //      window.defaultRouteName.contains('[sS]port')
 //      window.defaultRouteName.contains(new RegExp(r'[sS]port'))
       window.defaultRouteName
